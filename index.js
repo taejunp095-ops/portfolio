536ff3c1745a73ba3b7f0d7dfbcbd4286b72682a
taejunp095-ops <taejunp095@gmail.com>
2026-04-20 15:27:44 -0700

    lab4 step3

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
-// load all projects
+// ---------- Projects ----------
 const projects = await fetchJSON('./lib/projects.json');
 
-// take only first 3
-const latestProjects = projects.slice(0, 3);
-
-// find container on homepage
 const projectsContainer = document.querySelector('.projects');
 
-// render them
-if (latestProjects && projectsContainer) {
+if (projects && projectsContainer) {
+  const latestProjects = projects.slice(0, 3);
   renderProjects(latestProjects, projectsContainer, 'h2');
+}
+
+// ---------- GitHub ----------
+const githubData = await fetchGitHubData('taejunp095-ops'); // change if needed
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (githubData && profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
 }
